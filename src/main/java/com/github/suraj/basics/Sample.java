0b003aa5f06f4071e8d34272c824d10e325d6f50
suraj-nagaonkar <surajnagaonkar1994@gmail.com>
2022-02-13 19:53:50 +0530

    Add .gitignore file

---
+++ src/main/java/com/github/suraj/basics/Sample.java
@@ -0,0 +1,12 @@
+package com.github.suraj.basics;
+
+/**
+ * @author 09-02-2022
+ */
+
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello IDE!");
+    }
+}
